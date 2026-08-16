e31aa4bb5396007dbf5c60fb6908f7c826088ee0
HARIS QAMAR <harismemon422@gmail.com>
2026-08-16 23:26:52 +0500

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,12 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $host = "db.fr-roub1.bengt.wasmernet.com";
 $port = 20184;
 $username = "user_512b0390";
-$password = "pw_o0fCISQob5f0jRne14MxebdUBrgwGsOa";
+$password = "YOUR_NEW_PASSWORD";
 $database = "db_5c933e78";
 
 $conn = new mysqli($host, $username, $password, $database, $port);
@@ -13,5 +16,7 @@ if ($conn->connect_error) {
 }
 
 $conn->set_charset("utf8mb4");
+
+echo "Database connected successfully!";
 
 ?>
