89b9cbe9fe0ab5d41c412dd2aceb219bbb56f3f7
HARIS QAMAR <harismemon422@gmail.com>
2026-08-16 23:50:24 +0500

    Update db.php

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ $host = "db.fr-roub1.bengt.wasmernet.com";
 $port = 20184;
 $username = "user_58866cc0";
 $password = "pw_ucCGVi16A7h9Ao2IwI7CXo4ErKFTpHgD";
-$database = "portfolio_db";
+$database = "db_c2ae770c";
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
